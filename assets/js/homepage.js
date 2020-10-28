bfbc53c550374ab9ab8511be3ccf86d300c80d3c
Joshua Sung <yuniksung@gmail.com>
2020-10-27 23:50:15 -0500

    module finished

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,59 +2,88 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonEl = document.querySelector("#language-buttons")
 
-var formSubmitHandler = function(event) {
+    var formSubmitHandler = function(event) {
     event.preventDefault();
     var username = nameInputEl.value.trim();
     if (username) {
-        getUserRepos(username);
-        nameInputEl.value = "";
+    getUserRepos(username);
+    nameInputEl.value = "";
     } else {
-        alert("Please enter a GitHub username");
+    alert("Please enter a GitHub username");
     }
-}
-var displayRepos = function(repos, searchTerm) {
+    }
+    var displayRepos = function(repos, searchTerm) {
     if (repos.length === 0) {
-        repoContainerEl.textContent = "No repositories found.";
-        return;
+    repoContainerEl.textContent = "No repositories found.";
+    return;
     }
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
     for (var i = 0; i < repos.length; i++) {
-        var repoName = repos[i].owner.login + "/" + repos[i].name;
-        var repoEl = document.createElement("a");
-        repoEl.classList = "list-item flex-row justify-space-between align-center";
-        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
-        var titleEl = document.createElement("span");
-        titleEl.textContent = repoName;
-        repoEl.appendChild(titleEl);
-        var statusEl = document.createElement("span");
-        statusEl.classList = "flex-row align-center";
-        if (repos[i].open_issues_count > 0) {
-            statusEl.innerHTML =
-            "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-        } else {
-            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-        }
-        repoEl.appendChild(statusEl);
-        repoContainerEl.appendChild(repoEl);
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
+    var repoEl = document.createElement("a");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
+    repoEl.appendChild(titleEl);
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
+    if (repos[i].open_issues_count > 0) {
+        statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+    } else {
+        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
     }
-}
+    repoEl.appendChild(statusEl);
+    repoContainerEl.appendChild(repoEl);
+    }
+    }
 
 
-var getUserRepos = function(user) {
+    var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
     fetch(apiUrl).then(function(response) {
-        if (response.ok) { 
-        response.json().then(function(data) {
-            displayRepos(data, user);
-        });
+    if (response.ok) { 
+    response.json().then(function(data) {
+        displayRepos(data, user);
+    });
     }   else {
-        alert("Error: " + response.statusText);
+    alert("Error: " + response.statusText);
     }
     })
     .catch(function(error) {
-        alert("Unable to connect to GitHub");
+    alert("Unable to connect to GitHub");
     });
-};
-userFormEl.addEventListener("submit", formSubmitHandler);
+    };
+
+    var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then((response) => {
+    if(response.ok) {
+        response.json().then((data) => {
+            // console.log(data);
+            displayRepos(data.items, language);
+        })
+    } else {
+        alert("Error: " + response.statusText);
+    }
+    })
+    };
+
+    var buttonClickHandler = (event) =>{
+        var language = event.target.getAttribute("data-language")
+        // console.log(language);
+        if(language) {
+            getFeaturedRepos(language);
+
+            repoContainerEl.textContent = "";
+        }
+    }
+
+
+    userFormEl.addEventListener("submit", formSubmitHandler);
+    languageButtonEl.addEventListener("click", buttonClickHandler);
